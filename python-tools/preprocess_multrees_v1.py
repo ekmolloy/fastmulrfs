c93534066745e837f2ec1cb279be52d18833bee2
Erin  Molloy <ekmolloy@Erins-MacBook-Air-2.local>
2020-05-18 02:06:39 -0500

    Refactored code

--- python-tools/preprocess_multrees_v1.py
+++ python-tools/preprocess_multrees_v1.py
@@ -14,6 +14,122 @@ import dendropy
 import sys
 
 
+def is_binary(tree):
+    """
+    """
+    root = tree.seed_node
+    if len(root.child_nodes()) != 3:
+        sys.exit("Tree is not binary!")
+
+    for node in tree.preorder_node_iter():
+        if (node != root) and (not node.is_leaf()):
+            if len(node.child_nodes()) != 2:
+                sys.exit("Tree is not binary!")
+
+
+def build_down_profiles(tree, g2s_map):
+    """
+    """
+    for node in tree.postorder_node_iter():
+        if node.is_leaf():
+            gene = node.taxon.label
+            species = g2s_map[gene]
+            node.down_profile = set([species])
+            try:
+                g2s_map[species] = g2s_map[species] + [gene]
+            except KeyError:
+                g2s_map[species] = [gene]
+        else:
+            children = node.child_nodes()
+            if len(children) == 2:
+                [l, r] = children
+                node.down_profile = l.down_profile.union(r.down_profile)
+            else:
+                [l, m, r] = children
+                lr = l.down_profile.union(r.down_profile)
+                node.down_profile = lr.union(m.down_profile)
+
+
+def build_up_profiles(tree):
+    """
+    """
+    root = tree.seed_node
+    [rootl, rootm, rootr] = root.child_nodes()
+    rootl.up_profile = rootm.down_profile.union(rootr.down_profile)
+    rootm.up_profile = rootl.down_profile.union(rootr.down_profile)
+    rootr.up_profile = rootl.down_profile.union(rootm.down_profile)
+
+    for node in tree.preorder_node_iter():
+        if (node == root) or (node == rootl) or (node == rootm) or (node == rootr):
+            pass
+        elif node.is_leaf():
+            pass
+        else:
+            parent = node.parent_node
+            [pl, pr] = parent.child_nodes()
+            if node == pl:
+                sibling = pr.down_profile
+            else:
+                sibling = pl.down_profile
+            node.up_profile = parent.up_profile.union(sibling)
+
+
+def contract_edges_w_invalid_bipartitions(tree):
+    """
+    """
+    for node in tree.postorder_node_iter():
+        if node == tree.seed_node:
+            pass
+        elif node.is_leaf():
+            node.edge.length = 1.0
+        else:
+            test = node.down_profile.intersection(node.up_profile)
+            if len(test) != 0:
+                node.edge.length = 0.0
+            else:
+                node.edge.length = 1.0
+
+    tree.collapse_unweighted_edges()
+
+    for edge in tree.edges():
+        edge.length = None
+
+
+def prune_multiple_copies_of_species(tree, g2s_map):
+    """
+    """
+    for l in tree.leaf_nodes():
+        x = l.taxon.label
+        s = g2s_map[x]
+        if x != g2s_map[s][0]:
+            l.taxon = None
+
+    tree.prune_leaves_without_taxa()
+
+    # Relabel leaves
+    for l in tree.leaf_nodes():
+        x = l.taxon.label
+        l.taxon.label = g2s_map[x]
+
+
+def preprocess_multree(tree, g2s_map):
+    """
+    Preprocesses MUL-tree as described in the FastMulRFS paper
+    """
+    tree.is_rooted = False
+    tree.collapse_basal_bifurcation(set_as_unrooted_tree=True)
+
+    is_binary(tree)
+
+    build_down_profiles(tree, g2s_map)
+
+    build_up_profiles(tree)
+
+    contract_edges_w_invalid_bipartitions(tree)
+
+    prune_multiple_copies_of_species(tree, g2s_map)
+
+
 def read_g2s_map(ifil):
     """
     Parameters
@@ -38,20 +154,9 @@ def read_g2s_map(ifil):
     return g2sm
 
 
-def is_binary(tree):
-    root = tree.seed_node
-    if len(root.child_nodes()) != 3:
-        sys.exit("Tree is not binary!")
-
-    for node in tree.preorder_node_iter():
-        if (node != root) and (not node.is_leaf()):
-            if len(node.child_nodes()) != 2:
-                sys.exit("Tree is not binary!")
-
-
-def transform_multrees(ifil, mfil, ofil):
+def read_preprocess_and_write_multrees(ifil, mfil, ofil):
     """
-    Creates file for FastRFS as described in FastMulRFS paper
+    Creates file with preprocessed MUL-trees for FastRFS
 
     Parameters
     ----------
@@ -65,8 +170,7 @@ def transform_multrees(ifil, mfil, ofil):
     """
     ostr = ""
 
-    g2sm = read_g2s_map(mfil)
-    s2gm = {}
+    g2s_map = read_g2s_map(mfil)
 
     with open(ifil, 'r') as f:
         for line in f.readlines():
@@ -76,85 +180,7 @@ def transform_multrees(ifil, mfil, ofil):
                                      rooting='force-unrooted',
                                      preserve_underscores=True)
 
-            # Unroot tree
-            tree.is_rooted = False
-            tree.collapse_basal_bifurcation(set_as_unrooted_tree=True)
-
-            # Randomly root tree!
-            is_binary(tree)
-
-            # Create down profiles
-            for node in tree.postorder_node_iter():
-                if node.is_leaf():
-                    gene = node.taxon.label
-                    species = g2sm[gene]
-                    node.down_profile = set([species])
-                    try:
-                        s2gm[species] = s2gm[species] + [gene]
-                    except KeyError:
-                        s2gm[species] = [gene]
-                else:
-                    children = node.child_nodes()
-                    if len(children) == 2:
-                        [l, r] = children
-                        node.down_profile = l.down_profile.union(r.down_profile)
-                    else:
-                        [l, m, r] = children
-                        lr = l.down_profile.union(r.down_profile)
-                        node.down_profile = lr.union(m.down_profile)
-
-            # Create up profiles
-            root = tree.seed_node
-            [rootl, rootm, rootr] = root.child_nodes()
-            rootl.up_profile = rootm.down_profile.union(rootr.down_profile)
-            rootm.up_profile = rootl.down_profile.union(rootr.down_profile)
-            rootr.up_profile = rootl.down_profile.union(rootm.down_profile)
-
-            for node in tree.preorder_node_iter():
-                if (node == root) or (node == rootl) or (node == rootm) or (node == rootr):
-                    pass
-                elif node.is_leaf():
-                    pass
-                else:
-                    p = node.parent_node
-                    [pl, pr] = p.child_nodes()
-                    if node == pl:
-                        x = pr.down_profile
-                    else:
-                        x = pl.down_profile
-                    node.up_profile = p.up_profile.union(x)
-
-            # Contract edges
-            for node in tree.postorder_node_iter():
-                if node == root:
-                    pass
-                elif node.is_leaf():
-                    node.edge.length = 1.0
-                else:
-                    test = node.down_profile.intersection(node.up_profile)
-                    if len(test) != 0:
-                        node.edge.length = 0.0
-                    else:
-                        node.edge.length = 1.0
-
-            tree.collapse_unweighted_edges()
-
-            for edge in tree.edges():
-                edge.length = None
-
-            # Prune leaves
-            for l in tree.leaf_nodes():
-                x = l.taxon.label
-                s = g2sm[x]
-                if x != s2gm[s][0]:
-                    l.taxon = None
-
-            tree.prune_leaves_without_taxa()
-
-            # Relabel leaves
-            for l in tree.leaf_nodes():
-                x = l.taxon.label
-                l.taxon.label = g2sm[x]
+            preprocess_multree(tree, g2s_map)
 
             if len([l for l in tree.leaf_nodes()]) > 3:
                 ostr += tree.as_string(schema="newick")[5:]
@@ -171,7 +197,8 @@ def main(args):
         output = base[0] + "-for-fastrfs." + base[1]
     else:
         output = args.output
-    transform_multrees(args.input, args.map, output)
+
+    read_preprocess_and_write_multrees(args.input, args.map, output)
 
 
 if __name__ == '__main__':
